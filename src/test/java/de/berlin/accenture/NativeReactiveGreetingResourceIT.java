4262f97f8b12620fc7762e39c0de108918d7fae3
Xinhua Gu <xinhua.gu@gmail.com>
2022-04-14 09:55:34 +0200

    init

---
+++ src/test/java/de/berlin/accenture/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package de.berlin.accenture;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/de/berlin/accenture/ReactiveGreetingResourceTest.java
@@ -0,0 +1,21 @@
+package de.berlin.accenture;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class ReactiveGreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy Reactive"));
+    }
+
+}
