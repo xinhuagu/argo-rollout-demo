dee19404373f19bc4a3a740436231f9ab97a75e3
Xinhua Gu <xinhua.gu@gmail.com>
2022-04-14 10:00:05 +0200

    delete tests

--- src/test/java/de/berlin/accenture/NativeReactiveGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package de.berlin.accenture;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
